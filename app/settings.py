50e7c796a8784b4d251376b3e150db7413d6f1cc
LRP2004 <lsruizpena@gmail.com>
2025-10-24 03:40:41 -0400

    Change language to spanish

--- app/settings.py
+++ app/settings.py
@@ -106,12 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es-ES'
+TIME_ZONE = 'America/Caracas'
 USE_I18N = True
-
 USE_TZ = True
 
 
